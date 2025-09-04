5f153a54266500c85a55ca4c61627549cdb17a00
notjacklee <jacky0405.jl@gmail.com>
2025-09-04 19:27:44 +0800

    testing

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movies;
+use Illuminate\Http\Request;
+
+class MovieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Movies  $movies
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Movies $movies)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Movies  $movies
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Movies $movies)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Movies  $movies
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Movies $movies)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Movies  $movies
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Movies $movies)
+    {
+        //
+    }
+}
